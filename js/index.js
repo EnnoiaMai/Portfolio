48beff43f01e345a719eb71f75eafef0ec6fbf7f
Thuc <thuch.nguyen@yahoo.com>
2017-12-25 01:50:08 -0800

    Updated CSS of the homepage and added some content

--- js/index.js
+++ js/index.js
@@ -1,16 +1,40 @@
 
 $(document).ready(function() {
-
     setListeners()
 })
 
 function setListeners(){
-
     $("#languages_anchor").click(toggleSubMenu);
     $("#projects_anchor").click(toggleSubMenu);
+
+    // $('#link_syntax_html').on('click', changePage);
+    // $('#link_syntax_css').on('click', changePage);
+
+
+    initializePageContainer();
 }
 
 function toggleSubMenu() {
     //$(this).parent().find("ul").toggle();
     $(this).siblings("ul").toggle();
 }
+
+function initializePageContainer() {
+    updatePageContainer();
+}
+
+function updatePageContainer() {
+    var widthOfBrowserWindow = $(window).width();
+    var widthOfPageContainer = widthOfBrowserWindow - 200;
+    alert("width or browser window is: " + widthOfBrowserWindow + " and width of page container is " + widthOfPageContainer);
+    $('iframe').css({
+        "width": widthOfPageContainer + "px",
+        "height": "100%"
+    });
+}
+
+// function changePage(event) {
+//     // event.preventDefault();
+//     alert("Event id is = " + this.id);
+//
+// }
