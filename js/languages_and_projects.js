d55bbae98a76cc9cb7c93e540394b1bc87db33df
Thuc <thuch.nguyen@yahoo.com>
2017-12-27 01:16:07 -0800

    updated more CSS and JS

--- js/languages_and_projects.js
+++ js/languages_and_projects.js
@@ -3,7 +3,6 @@ Thuc Nguyen
 Date Created: December 2017
 */
 
-var linkSaver = new link_saver('enn');
 var submenuToggled = false;
 $(document).ready(function() {
     setListeners()
@@ -13,21 +12,18 @@ function setListeners(){
 
     $('#submenu img').on('click', toggleSubMenu);
     $('#sidebar a').on('click', updateMenu);
-    $('#sidebar a').on('click', saveLink);
     initializeMenu();
 }
-
 function toggleSubMenu() {
     $('#submenu_links').children("ul").toggle();
     if (!submenuToggled) {
-        $(this).attr("src", "images/close_submenu.png");
+        $(this).attr("src", "../images/close_submenu.png");
         submenuToggled = true;
     } else {
-        $(this).attr("src", "images/open_submenu.png");
+        $(this).attr("src", "../images/open_submenu.png");
         submenuToggled = false;
     }
 }
-
 function initializeMenu() {
     $("#sidebar a").addClass("normal_link");
 }
@@ -35,14 +31,3 @@ function updateMenu() {
     $('#sidebar a.highlighted_link').removeClass("highlighted_link");
     $(this).addClass("highlighted_link");
 }
-
-function saveLink() {
-    var id = this.id;
-    linkSaver.setCurrentLink(id);
-}
-
-// function changePage(event) {
-//     // event.preventDefault();
-//     alert("Event id is = " + this.id);
-//
-// }
