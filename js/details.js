2613bed7f17ddc629d90e2cd24d95372169e9b3d
Thuc Nguyen <thn079@ucsd.edu>
2018-09-01 03:12:10 -0700

    Media Query for youtube videos, selenium gui automation

--- js/details.js
+++ js/details.js
@@ -6,4 +6,18 @@ Date Created: December 2017
 $(document).ready(function() {
     currentPath = sidebarImagePath.PROJECT;
     initializeSidebar();
+
+    mediaQueryList.addListener(changeIframe);
 });
+
+function changeIframe() {
+    if (mediaQueryList.matches) {
+        // MOBILE
+        $("#youtube_selenium_console").replaceWith("<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/MztRBVEaBXs\" frameborder=\"0\" allow=\"autoplay; encrypted-media\" allowfullscreen id=\"youtube_selenium_console\"></iframe>");
+        $("#youtube_selenium_file").replaceWith("<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/_k8ZxoDWr6s\" frameborder=\"0\" allow=\"autoplay; encrypted-media\" allowfullscreen id=\"youtube_selenium_file\"></iframe>");
+    } else {
+        // DESKTOP
+        $("#youtube_selenium_console").replaceWith("<iframe width=\"971\" height=\"547\" src=\"https://www.youtube.com/embed/MztRBVEaBXs\" frameborder=\"0\" allow=\"autoplay; encrypted-media\" allowfullscreen id=\"youtube_selenium_console\"></iframe>");
+        $("#youtube_selenium_file").replaceWith("<iframe width=\"971\" height=\"547\" src=\"https://www.youtube.com/embed/_k8ZxoDWr6s\" frameborder=\"0\" allow=\"autoplay; encrypted-media\" allowfullscreen id=\"youtube_selenium_file\"></iframe>");
+    }
+}
