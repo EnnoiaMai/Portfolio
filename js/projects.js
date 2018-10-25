ebe5b7e85fb01e6d3273c1b559521bf54d24bb45
Thuc Nguyen <thn079@ucsd.edu>
2018-10-25 03:41:31 -0700

    Took out sun and moon, completed CSE11 Assignment 3 implementation

--- js/projects.js
+++ js/projects.js
@@ -6,4 +6,17 @@ Date Created: December 2017
 $(document).ready(function() {
     currentPath = sidebarImagePath.SYNTAX;
     initializeSidebar();
+
+    mediaQueryList.addListener(changeExternalLinkSize);
+    changeExternalLinkSize();
 });
+
+function changeExternalLinkSize() {
+    if (mediaQueryList.matches) {
+        // MOBILE
+        $(".external_link_container img").attr("src", "../images/ic_external_link_black_24dp.png");
+    } else {
+        // DESKTOP
+        $(".external_link_container img").attr("src", "../images/ic_external_link_black_36dp.png");
+    }
+}
